c8c40c4a556fb23fb2854b88c47247aa9ab6a7ff
frankie-tech <26461046+frankie-tech@users.noreply.github.com>
2021-08-25 15:09:45 -0400

    scope(feat) Added uhooks as a template for custom hooks system

--- snippets/elements.js
+++ snippets/elements.js
@@ -136,6 +136,56 @@ const requestAnimationTimeout = (fn, delay, cancelAnimationTimeout) => {
 	cancelAnimationTimeout(() => cancelAnimationFrame(raf));
 };
 
+// @TODO: Convert to a more custom version that does what we need
+const uhooks = ((h, o, O, K) => ({
+	hooked: (f) => {
+		const k = { i: 0, s: [] };
+		return function H() {
+			// const [p, r, e, v] = [h, o, O, K];
+			// [h, o, O, K] = [H, this, arguments, k];
+			const p = h,
+				r = o,
+				e = O,
+				v = K;
+			h = H;
+			o = this;
+			O = arguments;
+			K = k;
+			K.i = 0;
+			try {
+				return f.apply(o, O);
+			} finally {
+				h = p;
+				o = r;
+				O = e;
+				K = v;
+				// [h, o, O, K] = [p, r, e, v];
+			}
+		};
+	},
+	useRef: (current) => {
+		// const {i, s} = K;
+		if (K.i == K.s.length) K.s.push({ current: current });
+		return K.s[K.i++];
+	},
+	useState: (value) => {
+		// const [y, u, p, {i, s}] = [h, o, O, K];
+		const y = h,
+			u = o,
+			p = O,
+			i = K.i,
+			s = K.s;
+		if (i == s.length) s.push(value);
+		return [
+			s[K.i++],
+			(value) => {
+				s[i] = value;
+				y.apply(u, p);
+			},
+		];
+	},
+}))();
+
 export {
 	requestAnimationTimeout,
 	cancelAnimationFrameVariable,
